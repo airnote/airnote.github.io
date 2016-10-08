68406ea3947df8b76976e12c0ed219cba4786105
DaeHoon Zee <zeedhoon@gmail.com>
2016-10-08 14:36:48 +0900

    v 2.

---
+++ xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>http://54.249.14.94</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://54.249.14.94/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://54.249.14.94/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://54.249.14.94/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://54.249.14.94/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://54.249.14.94/wp-json/" />
+	    </apis>
+  </service>
+</rsd>
